144a8cefd54a0189669cd1442c371f22b43a92bd
orblancog <orblancog@gmail.com>
2016-10-03 14:30:12 +0200

    elipse at first flag

--- DrawProfile.C
+++ DrawProfile.C
@@ -14,8 +14,8 @@
 
 using namespace std;
 
-int DrawProfile () {
-
+int DrawProfile (char k[80]) {
+  cout << "  Using flag : "<< k << endl;
   //Beam geometrical emittances
   double_t ex = 1;
   double_t ey = 1;
@@ -129,20 +129,46 @@ int DrawProfile () {
   sigmay0  = TMath::Sqrt(ey*betay);
   sigmapy0 = TMath::Sqrt(ey/betay);
   sigmas0  = TMath::Sqrt(et*0);
+  offsetx0 = etax*Energyspread;
+  offsety0 = etay*Energyspread;
   //  sigmad0  = TMath::Sqrt(et*0);
 
-  TCanvas *c1 = new TCanvas("c1");
-  c1->Range(-20e-3,-20e-3,20e-3,20e-3);
-  c1->SetGrid(20,20);
-  TEllipse *el1 = new TEllipse(0,0,sigmax0,sigmay0);
-  TEllipse *el2 = new TEllipse(0,0,3*sigmax0,3*sigmay0);
+
+  Double_t w = 600;
+  Double_t h = 600;
+  TCanvas * c1 = new TCanvas("c1", "c1", w, h);
+  c1->SetWindowSize(w + (w - c1->GetWw()), h + (h - c1->GetWh()));
+  //  TCanvas *c1 = new TCanvas("c1");
+  c1->Range(-30e-3,-30e-3,30e-3,30e-3);
+  c1->SetFillColor(42);
+  //  c1->SetGrid(5,5);
+  c1->SetGridx(10);
+  c1->SetGridy(10);
+  c1->Update();
+  int x0[1]={0};
+  int y0[1]={0};
+  TGraph *gr = new TGraph(1,x0,y0);
+  gr->Draw("AC*");
+  gr->SetTitle(k);//input parameter
+  gr->GetXaxis()->SetLimits(-30e-3,30e-3);
+  gr->SetMinimum(-30e-3);
+  gr->SetMaximum(30e-3);
+  gr->GetXaxis()->CenterTitle();
+  gr->GetYaxis()->CenterTitle();
+  TEllipse *bpipe=new TEllipse(0,0,20e-3,20e-3);
+  bpipe->SetFillColorAlpha(kWhite,0.0);
+  TEllipse *el1 = new TEllipse(offsetx0,offsety0,sigmax0,sigmay0);
+  TEllipse *el2 = new TEllipse(offsetx0,offsety0,3*sigmax0,3*sigmay0);
 
   cout << "  Drawing ellipse of axes [m]: " << sigmax0 << ' ' << sigmapx0<<endl;
-  //  el1->GetXaxis()->SetNdivisions(20);
-  //  el1->GetYaxis()->SetNdivisions(20);
+  gr->GetXaxis()->SetNdivisions(10);
+  gr->GetYaxis()->SetNdivisions(10);
+  gr->GetXaxis()->SetTitle("x [m]");
+  gr->GetYaxis()->SetTitle("y [m]");
+  bpipe->Draw();
   el2->Draw();
   el1->Draw();
-  c1->RedrawAxis();
+  //  c1->RedrawAxis();
 
 
   return 0;
